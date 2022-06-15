df8367eabb7db88184217b6b9552b24d6d1e8731
KIMDONGHYEON <dhkim@bflysoft.com>
2022-06-15 17:38:30 +0900

    mysql 연결, 서버구축

--- src/main/java/kr/or/opengraph/controller/HomeController.java
+++ src/main/java/kr/or/opengraph/controller/HomeController.java
@@ -4,6 +4,7 @@ import kr.or.opengraph.lib.OpenGraph;
 import kr.or.opengraph.vo.OpenGraphVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,9 @@ public class HomeController {
 		
 		return "index";
 	}
-	
+
 	@ResponseBody
+	@CrossOrigin("*")
 	@GetMapping(value="/getOpenGraph")
 	public OpenGraphVO getOpenGraph(String baseURL) {
 		OpenGraphVO ogVO = null;
